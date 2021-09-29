582876fed2e35681d3ba731af5000459648b33c6
Philipp_ThinkBook <ai21m027@technikum-wien.at>
2021-09-29 18:17:40 +0200

    Add project files.

---
+++ exampleMLApp_GPU/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exampleMLApp_GPU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
